fe3245e1b9fa45fc45c5eed1d9f96640c6baf64d
dovela <892117745@qq.com>
2018-08-08 21:48:36 +0800

    更新cpu限制与矿池选择
    没测试，出bug提issue

--- OneClickMine.sh
+++ OneClickMine.sh
@@ -6,7 +6,7 @@ export PATH
 #=================================================
 #	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+/others(test)
 #	Description: Auto-install the ServerStatus Client
-#	Version: 1.0
+#	Version: 1.1
 #	Author: dovela
 #=================================================
 
@@ -58,22 +58,56 @@ set_http_port(){
     done
 }
 
+set_pool_address(){
+    echo -e "
+  1.pool.supportxmr.com:3333
+  2.xmr-us-east1.nanopool.org:14444
+  3.xmr-us-west1.nanopool.org:14444
+  4.xmr-eu1.nanopool.org:14444
+${sepa}
+  或手动输入地址 by manual
+  "
+  echo && stty erase '^H' && read -p " 矿池地址 Pool_address (默认 pool.supportxmr.com:3333 ):" x_address_num
+    case ${x_address_num} in
+        1)
+        x_address=pool.supportxmr.com:3333
+        ;;
+        2)
+        x_address=xmr-us-east1.nanopool.org:14444
+        ;;
+        3)
+        x_address=xmr-us-west1.nanopool.org:14444
+        ;;
+        4)
+        x_address=xmr-eu1.nanopool.org:14444
+        ;;
+        *)
+        if [[ -z ${x_address_num} ]]; then
+            x_address=pool.supportxmr.com:3333
+        else
+        x_address=${x_address_num}
+        fi
+        ;;
+    esac
+}
+
 set_xmr(){
     set_http_port
+    set_pool_address
     stty erase '^H' && read -p " 数字币名称 Currency (默认 monero7 ):" x_currency
     [[ -z ${x_currency} ]] && x_currency=monero7
-    stty erase '^H' && read -p " 矿池地址 Pool_address (默认 pool.supportxmr.com:3333 ):" x_address
-    [[ -z ${x_address} ]] && x_address=pool.supportxmr.com:3333
     stty erase '^H' && read -p " 钱包地址 Username (例: 49EJKgLMGCSFTEx5R7MTUPdhCrY8CjCiPMgLfRWcjh7eYf92f4FQ9PyCKDfBKNJ2EASBF9GB3yYeBKnVm4rGXhwG8ahAWdS ):" x_username
     [[ -z ${x_username} ]] && echo " Error, 钱包地址未输入!" && exit 1
     stty erase '^H' && read -p " 矿池密码 Password (例: do1:10000@qq.com ):" x_passwd
     stty erase '^H' && read -p " 矿机名称 Rig_ID (例: do1 ):" x_id
     stty erase '^H' && read -p " 矿池是否需要 TLS/SSL, 默认n (y/n):" x_tls
     [[ -z ${x_tls} ]] && x_tls=n
-    stty erase '^H' && read -p " 是否开启 Nicehash, 默认n (y/n):" x_nicehash
-    [[ -z ${x_nicehash} ]] && x_nicehash=n
-    stty erase '^H' && read -p " 是否开启 Multiple pools, 默认n (y/n):" x_multiple
-    [[ -z ${x_multiple} ]] && x_multiple=n
+ #   stty erase '^H' && read -p " 是否开启 Nicehash, 默认n (y/n):" x_nicehash
+ #   [[ -z ${x_nicehash} ]] && x_nicehash=n
+    x_nicehash=n
+ #   stty erase '^H' && read -p " 是否开启 Multiple pools, 默认n (y/n):" x_multiple
+ #   [[ -z ${x_multiple} ]] && x_multiple=n
+    x_multiple=n
     x_text=" browser interface port: "${x_port}"\n Currency: "${x_currency}"\n Pool address: "${x_address}"\n Username: "${x_username}"\n Password: "${x_passwd}"\n Rig ID: "${x_id}"\n TLS/SSL: "${x_tls}"\n Nicehash: "${x_nicehash}"\n Multiple pools: "${x_multiple}
     clear
     echo -e "${sepa}\n${x_text}\n${sepa}"
@@ -105,13 +139,13 @@ View_conf(){
 
 centos_yum(){
 	yum install -y epel-release && yum clean all && yum update
-	yum install -y git wget libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev wget
+	yum install -y git wget libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev wget cpulimit
 }
 
 debian_apt(){
     apt-get update
     apt-get upgrade -y
-    apt install -y libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev git wget
+    apt install -y libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev git wget cpulimit
 }
 
 install_env(){
@@ -143,6 +177,7 @@ Install_xmr(){
     cd ${xmr_folder}
     nohup ./xmr-stak -i ${x_port} -o ${x_address} -u ${x_username} -r ${x_id} -p ${x_passwd} --currency ${x_currency} &>/dev/null &
     echo ' 配置完毕, xmr-stak 已启动...'
+    Limit_xmr
 }
     
 Run_xmr(){
@@ -152,6 +187,7 @@ Run_xmr(){
     nohup ./xmr-stak &> /dev/null &
     check_PID
     [[ -n ${PID} ]] && echo ' xmr-stak 已启动 !'
+    Limit_xmr
 }
 
 Stop_xmr(){
@@ -168,6 +204,29 @@ Remove_xmr(){
     rm -rf ${file}
 }
 
+Limit_xmr(){
+    cpu_core=`cat /proc/cpuinfo | grep "processor"| wc -l`
+    cpu_percentage_total=`expr ${cpu_core} \* 100`
+    check_PID
+    if [[ -n ${PID} ]]; then
+        echo ' 限制 xmr-stak 占用cpu总百分比 (逻辑cpu数 * 100), 默认不限制'
+        stty erase '^H' && read -p " 请输入[1-${cpu_percentage_total}]间的整数 (例: 50): " x_limit
+        [[ -z ${x_limit} ]] && echo ' 不限制 xmr-stak cpu占用率' && exit 1
+        expr x_limit + 0 &>/dev/null
+        if [[ $? == 0 ]], then
+            if [[ x_limit -ge 1 ]] && [[ x_limit -le ${cpu_percentage_total} ]], then
+            nohup cpulimit -p ${PID} -l ${x_limit} &>/dev/null &
+            else
+                echo ' Error, 请输入正确的整数!'
+            fi
+        else
+            echo ' Error, 请输入正确的整数!'
+        fi
+    else
+        echo ' Error, xmr-stak 未在运行! 请运行后再执行此项'
+    fi
+}
+
 clear
 check_sys
 [ $(id -u) != "0" ] && echo -e "Error: You must be root to run this script" && exit 1
@@ -180,10 +239,11 @@ ${sepa}
 ${sepa}
   5.查看当前配置
   6.重新配置
+  7.限制 xmr-stak cpu占用率
 ${sepa}
   输入数字开始，或ctrl + c退出
 "
-echo && stty erase '^H' && read -p " 请输入数字[1-6]:" num
+echo && stty erase '^H' && read -p " 请输入数字[1-7]:" num
  case "$num" in
     1)
     Install_xmr
@@ -203,7 +263,10 @@ echo && stty erase '^H' && read -p " 请输入数字[1-6]:" num
     6)
     Reset_xmr
     ;;
+    7)
+    Limit_xmr
+    ;;
     *)
-    echo -e "Error, 请输入正确的数字 [1-6]!"
+    echo -e "Error, 请输入正确的数字 [1-7]!"
 	;;
 esac
--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 # 支持的系统
 Ubuntu18.04.1  Ubuntu16.04.5 实测通过
 <br>Centos7 和 Debian8 也做了适配，应该可行，未测试
+<br>目前仅支持cpu挖矿，可以自己改脚本使用gpu
 
 # 使用方法
 ```
@@ -11,9 +12,15 @@ wget -N --no-check-certificate https://raw.githubusercontent.com/dovela/xmr-stak
 chmod +x OneClickMine.sh
 ./OneClickMine.sh
 ```
-默认矿池地址 [https://www.supportxmr.com](https://www.supportxmr.com)
+#特别说明
+默认矿池地址 [https://www.supportxmr.com](https://www.supportxmr.com) 或 [https://xmr.nanopool.org/](https://xmr.nanopool.org/)
 <br>所需配置可以从 [https://www.supportxmr.com/#/help/config_generator](https://www.supportxmr.com/#/help/config_generator) 中查看
+
+<br>使用vps挖矿请遵守服务商使用条款，因挖矿造成的一切收益和损失与本人无关
+<br>珍爱vps，远离挖矿，血的教训
 
 # 感谢
 @Toyo [https://github.com/ToyoDAdoubi](https://github.com/ToyoDAdoubi)
 <br>及许多贡献自己知识的博客
+<br>另：求求大矿场赏口饭吃吧，本人钱包49EJKgLMGCSFTEx5R7MTUPdhCrY8CjCiPMgLfRWcjh7eYf92f4FQ9PyCKDfBKNJ2EASBF9GB3yYeBKnVm4rGXhwG8ahAWdS
+
